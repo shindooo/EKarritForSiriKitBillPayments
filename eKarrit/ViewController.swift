4e4c0d7991dfd6ae293b1ab2a733bfff2f938e4d
shindooo <tks.shindou@live.jp>
2017-04-23 17:25:16 +0900

    Initial Commit

---
+++ eKarrit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eKarrit
+//
+//  Created by 進藤 孝司 on 2017/04/23.
+//  Copyright © 2017年 T.Shindou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
